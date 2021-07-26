c28effaee9f763d0a2db734a14640b4fe8917bb4
Will Ross <paxswill@paxswill.com>
2021-07-26 18:20:17 -0400

    Add image size constants for MLX90641

---
+++ src/mlx90641/mod.rs
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright © 2021 Will Ross
+
+/// The height of the image captured by sensor in pixels.
+pub(crate) const HEIGHT: usize = 12;
+
+/// The width of the image captured by the sensor in pixels.
+pub(crate) const WIDTH: usize = 16;
+
+/// The total number of pixels an MLX90640 has.
+pub(crate) const NUM_PIXELS: usize = HEIGHT * WIDTH;
+
