681d146eb26842ac6e6807186a908605c72a4f06
Will Ross <paxswill@paxswill.com>
2021-07-29 11:12:55 -0400

    Add enum for MLX90641 EEPROM addresses
    Not that it's used much in this crate

--- src/mlx90641/address.rs
+++ src/mlx90641/address.rs
@@ -7,6 +7,117 @@ use num_enum::{IntoPrimitive, TryFromPrimitive, TryFromPrimitiveError};
 use crate::address_enum_ops;
 use crate::common::Address;
 
+#[derive(Clone, Copy, Debug, Eq, PartialEq, PartialOrd, Ord, IntoPrimitive, TryFromPrimitive)]
+#[repr(u16)]
+#[allow(non_camel_case_types)]
+pub(super) enum EepromAddress {
+    /// The start of the EEPROM address range
+    ///
+    /// For calibration purposes, the first 16 words (word size is 16 bits) can be ignored.
+    Base = 0x2400,
+
+    /// The bits 5 through 10 are the offset compensation scaling factor.
+    OffsetCompensationScale = 0x2410,
+
+    /// The pixel offset average is split across two words.
+    OffsetCompensationAverageStart = 0x2411,
+
+    K_TaAverage = 0x2415,
+
+    K_TaScale = 0x2416,
+
+    K_VAverage = 0x2417,
+
+    K_VScale = 0x2418,
+
+    PixelSensitivityScaleStart = 0x2419,
+
+    PixelSensitivityMaxStart = 0x241C,
+
+    KsTa = 0x2422,
+
+    Emissivity = 0x2423,
+
+    GainStart = 0x2424,
+
+    Vdd25 = 0x2426,
+
+    K_Vdd = 0x2427,
+
+    PtatStart = 0x2428,
+
+    KtPtat = 0x242A,
+
+    KvPtat = 0x242B,
+
+    AlphaPtat = 0x242C,
+
+    CompensationPixelSensitivity = 0x242D,
+
+    CompensationPixelSensitivityScale = 0x242E,
+
+    CompensationPixelOffsetStart = 0x242F,
+
+    CompensationPixelKt = 0x2431,
+
+    CompensationPixelKv = 0x243,
+
+    ThermalGradientCompensation = 0x2433,
+
+    KsToScale = 0x2434,
+
+    KsTo0 = 0x2435,
+
+    KsTo1 = 0x2436,
+
+    KsTo2 = 0x2437,
+
+    KsTo3 = 0x2438,
+
+    KsTo4 = 0x2439,
+
+    CornerTemperature5 = 0x243A,
+
+    KsTo5 = 0x243B,
+
+    CornerTemperature6 = 0x243C,
+
+    KsTo6 = 0x243D,
+
+    CornerTemperature7 = 0x243E,
+
+    KsTo7 = 0x243F,
+
+    PixelOffsetSubpage0Start = 0x2440,
+
+    PixelSensitivityStart = 0x2500,
+
+    PixelConstantsStart = 0x25C0,
+
+    PixelOffsetSubpage1Start = 0x2680,
+
+    /// The last valid address in the MLX90641 EEPROM.
+    End = 0x273F,
+}
+
+address_enum_ops!(EepromAddress);
+
+impl TryFrom<Address> for EepromAddress {
+    type Error = TryFromPrimitiveError<EepromAddress>;
+
+    fn try_from(value: Address) -> Result<Self, Self::Error> {
+        let raw_address: u16 = value.into();
+        Self::try_from(raw_address)
+    }
+}
+
+impl From<EepromAddress> for Address {
+    fn from(eeprom_address: EepromAddress) -> Self {
+        let raw_address: u16 = eeprom_address.into();
+        raw_address.into()
+    }
+}
+
 /// Significant RAM addresses for the MLX909641.
 #[derive(Clone, Copy, Debug, Eq, PartialEq, PartialOrd, Ord, IntoPrimitive, TryFromPrimitive)]
 #[repr(u16)]
